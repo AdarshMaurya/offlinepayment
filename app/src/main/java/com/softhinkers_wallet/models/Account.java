2181925d268da6cf3d47f3d03ab1447c17ca6ba4
AdarshMaurya <adarshmauryatalks@gmail.com>
2017-05-16 22:13:19 +0530

    -initial commit

---
+++ app/src/main/java/com/softhinkers_wallet/models/Account.java
@@ -0,0 +1,13 @@
+package com.softhinkers_wallet.models;
+
+/**
+ * Created by adarsh on 05/04/17.
+ */
+public class Account {
+    public String ownerKey;
+    public String name;
+    public String activeKey;
+    public String referrer;
+    public String refcode;
+    public String memoKey;
+}
